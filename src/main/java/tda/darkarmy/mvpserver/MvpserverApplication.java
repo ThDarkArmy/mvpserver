4eab36feab7e23bf8420cf8925cfb2e06dcc3810
Prashant Raj <darkarmy@Prashants-MacBook-Pro.local>
2025-06-03 01:12:17 +0530

    app

--- src/main/java/tda/darkarmy/mvpserver/MvpserverApplication.java
+++ src/main/java/tda/darkarmy/mvpserver/MvpserverApplication.java
@@ -32,4 +32,29 @@ public class MvpserverApplication {
 	}
 */
 
+	/*
+server {
+    listen 80;
+    server_name first-buy.in www.first-buy.in;
+    return 301 https://$host$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name first-buy.in www.first-buy.in;
+
+    ssl_certificate /etc/letsencrypt/live/first-buy.in/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/first-buy.in/privkey.pem;
+
+    location / {
+        proxy_pass http://localhost:8080;
+
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+	* */
+
 }
